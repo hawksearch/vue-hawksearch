7da228fa6c198510f0609e4b8c58625e10a61803
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2020-04-24 10:33:19 +0300

    Update babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,10 @@ module.exports = {
                         plugins: [
                             '@babel/plugin-proposal-object-rest-spread',
                             '@babel/plugin-syntax-dynamic-import',
-                            '@babel/plugin-proposal-class-properties'
+                            '@babel/plugin-proposal-class-properties',
+                            'babel-plugin-transform-export-extensions',
+                            '@babel/plugin-proposal-export-default-from',
+                            'babel-plugin-transform-es2015-modules-commonjs'
                         ]
                     }
                 }
