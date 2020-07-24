8097f820ae731ba8725eed4a7afe45d16f22af11
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2020-07-24 17:36:17 +0300

    Updated suggestions

---
+++ src/components/search-box/PopularContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="suggestions && suggestions.Popular && suggestions.Popular.length">
+        <h3>{{ suggestions.PopularHeading }}</h3>
+        <li v-for="popular in suggestions.Popular" :key="popular.Value" class="autosuggest-menu__item">
+            <div v-html="popular.Value"></div>
+        </li>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'popular-container',
+        props: ['suggestions'],
+        mounted() {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+    }
+
+</script>
+
+<style scoped lang="scss">
+</style>
