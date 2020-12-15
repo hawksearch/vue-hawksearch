42f39986c026d6bd669ab28c25d685263f523a88
liliya.burlakova <liliya.burlakova@BG-1WS130>
2020-12-15 11:45:46 +0200

    Adding a CustomResultsLabel component

---
+++ src/components/results/tools/CustomResultsLabel.vue
@@ -0,0 +1,27 @@
+<template>
+    <span>{{ getResponseField(resultsField) }}</span>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+
+    export default {
+        props: ['resultsField'],
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+            
+        },
+        computed: {
+            ...mapGetters([
+                'getResponseField'
+            ])
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+</style>
