a77ca3e48592aa4b99e5ed64f015d2ca2a652d30
metodi.matov@americaneagle.com <metodi.matov@americaneagle.com>
2021-04-12 12:08:10 +0300

    Adding language selector component

---
+++ src/components/language-selector/LanguageSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="language-selector">
+    <span>{{ resultsLanguageLabel }}</span>
+    <span v-for="(language, index) in languages" :key="language">
+      <span @click="updateUrl(language.languageId)" class="language">{{ language.languageName }}</span>
+      <span v-if="index < languages.length - 2">, </span>
+      <span v-else-if="index == languages.length - 2">{{orLabel}}&nbsp;</span>
+    </span>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import { parseSearchQueryString } from '../../QueryString';
+export default {
+  name: "language-selector",
+  props: ["languageUri", "urlPath", "resultsLanguageLabel", "orLabel"],
+  components: {},
+  created() {},
+  mounted() {
+    this.languages = this.convertDictionaryToObject(
+      JSON.parse(this.$root.$store.state.config.languages)
+    );
+   
+    this.indexName = this.$root.$store.state.config.indexName;
+  },
+  data() {
+    return {
+      languages: null,
+      indexName: null,
+      urlPath: this.urlPath,
+    };
+  },
+  methods: {
+    updateUrl: function (language) {
+        this.$root.$store.commit("updateLanguage", language);
+        var searchQuery = this.indexName != null && this.indexName !="" ? "?language=" + language : "?language="+ language +"&indexName="+this.indexName;
+        var searchParams = parseSearchQueryString(searchQuery);
+        this.$root.dispatchToStore('fetchResults', searchParams).then(() => {
+            HawksearchVue.truncateFacetSelections(this.$root.$store);
+            HawksearchVue.applyTabSelection(this.$root);
+        });
+    },
+    convertDictionaryToObject: function (languages) {
+      var languagesArray = [];
+      for (const [key, value] of Object.entries(languages)) {
+        languagesArray.push({ languageId : key,  languageName:value });
+      }
+      return languagesArray;
+    },
+  },
+  computed: {
+    ...mapState([]),
+    ...mapGetters([]),
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
