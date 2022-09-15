394f692eab6822d19910566f41e2822432de7788
chriswudev <chris.wu.tiff@gmail.com>
2022-09-15 10:23:58 -0400

    HS-1916: Vue SDK SearchSuggestions component - Invalid HTML

--- src/components/search-box/PopularContainer.vue
+++ src/components/search-box/PopularContainer.vue
@@ -1,9 +1,9 @@
 <template>
     <div v-if="suggestions && suggestions.Popular && suggestions.Popular.length">
         <h3>{{ suggestions.PopularHeading }}</h3>
-        <li v-for="popular in suggestions.Popular" :key="popular.Value" class="autosuggest-menu__item">
+        <div v-for="popular in suggestions.Popular" :key="popular.Value" class="autosuggest-menu__item">
             <div v-html="popular.Value"></div>
-        </li>
+        </div>
     </div>
 </template>
 
