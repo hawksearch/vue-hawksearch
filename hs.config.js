1395c93a91c95e851fd1900aaa2b21d0416ace34
Alexander Martinkevich <alexandr.martinkevich@h1.nl>
2026-04-25 18:40:30 +0300

    feat(build): update build output formats and browser targets

--- hs.config.js
+++ hs.config.js
@@ -4,8 +4,8 @@ function getClientGuidFromUrl() {
 }
 
 export default {
-    clientGuid: getClientGuidFromUrl() || import.meta.env.VITE_CLIENT_GUID || "3953cf87e0aa4a4eb9496a24d5a0fa84",
-    apiUrl: import.meta.env.VITE_API_URL || "https://searchapi-dev.hawksearch.net/api/v2/search/",
+    clientGuid: getClientGuidFromUrl() || import.meta.env.VITE_CLIENT_GUID || "",
+    apiUrl: import.meta.env.VITE_API_URL || "",
     appName: import.meta.env.VITE_APP_NAME,
     generateTemplateOverrides: true
 };
